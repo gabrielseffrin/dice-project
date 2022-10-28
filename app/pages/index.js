5d55e7c38ac6943c636a6084c22b656fe010783d
gabrielseffrin <gabrielfseffrin@gmail.com>
2022-10-27 23:14:20 -0300

    deploy

--- app/pages/index.js
+++ app/pages/index.js
@@ -23,40 +23,40 @@ let rollDice = () => {
     switch (typeDice) {
         case 1:
             result.innerHTML = quantityDice(quantity, 4);
-            imgDice.src = 'assets/images/dice-d4.png';
+            imgDice.src = '/assets/images/dice-d4.png';
             break;
         case 2:
             result.innerHTML = quantityDice(quantity, 6);
-            imgDice.src = 'assets/images/dice-d6.png';
+            imgDice.src = '/assets/images/dice-d6.png';
 
             break;
         case 3:
             result.innerHTML = quantityDice(quantity, 8);
-            imgDice.src = 'assets/images/dice-d8.png';
+            imgDice.src = '/assets/images/dice-d8.png';
 
             break;
         case 4:
             result.innerHTML = quantityDice(quantity, 10);
-            imgDice.src = 'assets/images/dice-d10.png';
+            imgDice.src = '/assets/images/dice-d10.png';
 
             break;
         case 5:
             result.innerHTML = quantityDice(quantity, 12);
-            imgDice.src = 'assets/images/dice-d12.png';
+            imgDice.src = '/assets/images/dice-d12.png';
 
             break;
         case 6:
             result.innerHTML = quantityDice(quantity, 20);
-            imgDice.src = 'assets/images/dice-d20.png';
+            imgDice.src = '/assets/images/dice-d20.png';
 
             break;
         case 7:
             result.innerHTML = quantityDice(quantity, 100);
-            imgDice.src = 'assets/images/dice-d10.png';
+            imgDice.src = '/assets/images/dice-d10.png';
 
             break;
         default:
-            alert('error');
+            alert('falta informação!');
             break;
     }
 };
